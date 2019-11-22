448af60358f4031ffd25b2d0df75d1312615a975
Will Yelton <willyelton123@gmail.com>
2019-11-22 12:18:53 -0700

    more class building

--- FinalPROJ/hashClass.cpp
+++ FinalPROJ/hashClass.cpp
@@ -9,15 +9,17 @@ class Hash{
     ~Hash();
     int hashMod(int key);
     int hashFloor(int key);
+    void printTable(int number);
   private:
-    int TABLE_SIZE;
-
-
+    static const int TABLE_SIZE = 1019;
+    int hashTable1[TABLE_SIZE];
+    int hashTable2[TABLE_SIZE];
 
 };
 Hash::Hash(){
-  TABLE_SIZE = 1019; //Project Writeup
-  //cout << TABLE_SIZE << endl;
+  for(int i = 0; i < TABLE_SIZE; i++){
+  hashTable2[i] = hashTable1[i] = -1;
+  }
 }
 Hash::~Hash(){
 
@@ -28,8 +30,23 @@ int Hash::hashMod(int key){
 int Hash::hashFloor(int key){
   return key/TABLE_SIZE; //Uneven int division just returns floor
 }
+void Hash::printTable(int number){
+    if(number == 1){
+      for(int i = 0; i < TABLE_SIZE; i++){//print table 1
+        cout << i << " : " << hashTable1[i] << endl;
+
+      }
+      return;
+    }
+    for(int p = 0; p < TABLE_SIZE; p++){ //print table 2
+      cout << hashTable2[p] << " ";
+    }
+return ;
+}
 
 int main(){
   Hash h1;
-  cout << h1.hashFloor(5221) << endl;//Expected 5
+  //cout << h1.hashFloor(5221) << endl;//Expected 5
+
+  h1.printTable(1);
 }
