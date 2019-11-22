721d5978f07e99ce1a72dc1a6712e02814eba3bd
Will Yelton <willyelton123@gmail.com>
2019-11-22 12:38:18 -0700

    starting linear collision solution

--- FinalPROJ/hashClass.cpp
+++ FinalPROJ/hashClass.cpp
@@ -2,6 +2,14 @@
 #include <string>
 #include <fstream>
 using namespace std;
+struct LLNode{ //Linked List collision function
+  int key ;
+  LLNode* next;
+};
+struct TreeNode{ //BST collision struct
+  int key;
+  TreeNode *left , *right;
+};
 class Hash{
 
   public:
@@ -10,6 +18,8 @@ class Hash{
     int hashMod(int key);
     int hashFloor(int key);
     void printTable(int number);
+    bool table1Linear(int index, int key);
+    bool table2Linear(int index, int key);
   private:
     static const int TABLE_SIZE = 1019;
     int hashTable1[TABLE_SIZE];
@@ -42,6 +52,23 @@ void Hash::printTable(int number){
       cout << hashTable2[p] << " ";
     }
 return ;
+}
+bool Hash::table1Linear(int index, int key){
+    for(int i = index; i < TABLE_SIZE; i++){
+      if(hashTable1[i] == -1) //If empty index
+      hashTable1table[i] = key; //Place new data
+      return true;
+    }
+    return false; //If table full
+}
+bool Hash::table2Linear(int index, int key){ 
+  for(int i = index; i < TABLE_SIZE; i++){
+    if(hashTable2[i] == -1) //If empty index
+    hashTable2[i] = key; //Place new data
+    return true;
+  }
+  return false; //If table full
+}
 }
 
 int main(){
